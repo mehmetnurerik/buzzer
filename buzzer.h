e00709378c7b9730f6042049d2873c09ec532b1d
mehmetnurerik <47780650+mehmetnurerik@users.noreply.github.com>
2019-02-26 09:59:34 +0300

    Update buzzer.h

--- buzzer.h
+++ buzzer.h
@@ -1,13 +1,307 @@
 #ifndef buzzer_h // tanımlayıcı kutuphane ismimiz motor
-#define buzzer_h
+#define buzzer
+#define a=440;
+#define ad=466;
+#define b=494;
+
+#define c=523;
+#define cd=554;
+#define d=587;
+#define dd=622;
+#defin e=659;
+#define f = 698;
+#define fd = 740;
+#define g = 784;
+#define gd = 830;
+
+#define a2 = 880;
+#define ad2 = 932;
+#define b2 = 988;
+#define c2 = 1046;
+#define cd2 = 1108;
+#define d2 = 1174;
+#define dd2 = 1244;
+#define e2 = 1318;
+#define f2 = 1396;
+#define fd2 = 1480;
+#define g2 = 1568;
+#define gd2 = 1660;
+#define a3 = 1760;
+
  
 class istiklal{  //motorclass isminde class oluşturuyoruz
   public: // heryerden erişilebilir olması için public
-    motorclass(); //motorclass isminde fonksiyon oluşturuyoruz
+    istiklal(); //motorclass isminde fonksiyon oluşturuyoruz
     void SETUP();  //setup isminde fonksiyon oluşturuyoruz
-    void HIZ(int motor_hizi); // hız fonksiyonu
+ tone(buzzer, c);
+    delay(800);
+    tone(buzzer, f);
+    delay(800);
+    tone(buzzer, g);
+    delay(800);
+    tone(buzzer, gd);
+    delay(800);
+    tone(buzzer, e);
+    delay(400);
+    tone(buzzer, g);
+    delay(200);        
+    tone(buzzer, f);
+    delay(1600);        
+    noTone(buzzer);
+    delay(300);
+//Larda Yüzden Al Sancak
+    tone(buzzer, f);
+    delay(800);        
+    tone(buzzer, ad2);
+    delay(800);        
+    tone(buzzer, c2);
+    delay(800);        
+    tone(buzzer, cd2);
+    delay(800);
+    tone(buzzer, a2);
+    delay(400);
+    tone(buzzer, c2);
+    delay(200);
+    tone(buzzer, ad2);
+    delay(1600);        
+//Sönmeden Yurdumun Üstünde Tüten En Son Ocak O Be
+    tone(buzzer, c2);
+    delay(200);        
+    tone(buzzer, ad2);
+    delay(200);        
+    tone(buzzer, c2);
+    delay(200);        
+    tone(buzzer, g);
+    delay(400);        
+    noTone(buzzer);
+    delay(100);  
+    tone(buzzer, g);
+    delay(400);        
+    tone(buzzer, ad);
+    delay(200);
+    tone(buzzer, gd);
+    delay(400);   
+    tone(buzzer, e);
+    delay(200);
+    tone(buzzer, f);
+    delay(400);   
+    tone(buzzer, g);
+    delay(200);
+    tone(buzzer, gd);
+    delay(400);        
+    tone(buzzer, ad);
+    delay(200);        
+    tone(buzzer, c2);
+    delay(400);        
+    tone(buzzer, cd2);
+    delay(200);        
+    tone(buzzer, dd2);
+    delay(400);        
+    tone(buzzer, f2);
+    delay(200);        
+    tone(buzzer, dd2);
+    delay(400);     
+//Nim Milletimin
+    tone(buzzer, dd);
+    delay(200);
+    tone(buzzer, d);
+    delay(200);
+    tone(buzzer, dd);
+    delay(200);
+    tone(buzzer, c2);
+    delay(800);
+    tone(buzzer, ad);
+    delay(800);
+    tone(buzzer, gd);
+    delay(1600);
+// Yıldızıdır Parlayacak O benim
+    tone(buzzer, c);
+    delay(200);
+    tone(buzzer, b);
+    delay(200);
+    tone(buzzer, c);
+    delay(200);
+    tone(buzzer, g);
+    delay(800);
+    tone(buzzer, c);
+    delay(800);
+    tone(buzzer, c2);
+    delay(800);
+    tone(buzzer, ad);
+    delay(200);
+    tone(buzzer, gd);
+    delay(200);
+    tone(buzzer, g);
+    delay(400);
+    tone(buzzer, gd);
+    delay(200);
+    tone(buzzer, f);
+    delay(800);
+//Dir O Benim Milletimindir Ancak
+    tone(buzzer, f2);
+    delay(800);
+    tone(buzzer, dd2);
+    delay(200);
+    tone(buzzer, cd2);
+    delay(400);
+    tone(buzzer, c2);
+    delay(200);
+    tone(buzzer, ad);
+    delay(400);
+    tone(buzzer, gd);
+    delay(200);
+    tone(buzzer, g);
+    delay(400);
+    tone(buzzer, f);
+    delay(200);
+    tone(buzzer, c2);
+    delay(400);
+    tone(buzzer, c);
+    delay(800);
+    tone(buzzer, f);
+    delay(1600);
+
+/////////////ikinci kıt'a
+
+//Çatma Kurban Olayım
+    tone(buzzer, c);
+    delay(800);
+    tone(buzzer, f);
+    delay(800);
+    tone(buzzer, g);
+    delay(800);
+    tone(buzzer, gd);
+    delay(800);
+    tone(buzzer, e);
+    delay(400);
+    tone(buzzer, g);
+    delay(200);        
+    tone(buzzer, f);
+    delay(1600);        
+    noTone(buzzer);
+    delay(300);
+//Çehreni Ey Nazlı Hilal
+    tone(buzzer, f);
+    delay(800);        
+    tone(buzzer, ad2);
+    delay(800);        
+    tone(buzzer, c2);
+    delay(800);        
+    tone(buzzer, cd2);
+    delay(400);
+    tone(buzzer, ad2);
+    delay(400);
+    tone(buzzer, a2);
+    delay(400);
+    tone(buzzer, c2);
+    delay(400);
+    tone(buzzer, ad2);
+    delay(800);        
+//Kahraman Irkıma Bir Gül Ne Bu Şiddet Bu Celal Sana
+    tone(buzzer, c2);
+    delay(200);        
+    tone(buzzer, ad2);
+    delay(200);        
+    tone(buzzer, c2);
+    delay(200);        
+    tone(buzzer, g);
+    delay(400);        
+    noTone(buzzer);
+    delay(100);  
+    tone(buzzer, g);
+    delay(400);        
+    tone(buzzer, ad);
+    delay(200);
+    tone(buzzer, gd);
+    delay(400);   
+    tone(buzzer, e);
+    delay(200);
+    tone(buzzer, f);
+    delay(400);   
+    tone(buzzer, g);
+    delay(200);
+    tone(buzzer, gd);
+    delay(400);        
+    tone(buzzer, ad);
+    delay(200);        
+    tone(buzzer, c2);
+    delay(400);        
+    tone(buzzer, cd2);
+    delay(200);        
+    tone(buzzer, dd2);
+    delay(400);        
+    tone(buzzer, f2);
+    delay(200);        
+    tone(buzzer, dd2);
+    delay(400);     
+//Olmaz Dökülen
+    tone(buzzer, dd);
+    delay(200);
+    tone(buzzer, d);
+    delay(200);
+    tone(buzzer, dd);
+    delay(200);
+    tone(buzzer, c2);
+    delay(800);
+    tone(buzzer, ad);
+    delay(800);
+    tone(buzzer, gd);
+    delay(1600);
+//Kanlarımız Sonra Helal Hakkıdır
+    tone(buzzer, c);
+    delay(200);
+    tone(buzzer, b);
+    delay(200);
+    tone(buzzer, c);
+    delay(200);
+    tone(buzzer, g);
+    delay(800);
+    tone(buzzer, c);
+    delay(800);
+    tone(buzzer, c2);
+    delay(800);
+    tone(buzzer, ad);
+    delay(200);
+    tone(buzzer, gd);
+    delay(200);
+    tone(buzzer, g);
+    delay(400);
+    tone(buzzer, gd);
+    delay(200);
+    tone(buzzer, f);
+    delay(800);
+//Hakk'a Tapan Milletimin İstiklal
+    tone(buzzer, f2);
+    delay(800);
+    tone(buzzer, dd2);
+    delay(200);
+    tone(buzzer, cd2);
+    delay(400);
+    tone(buzzer, c2);
+    delay(200);
+    tone(buzzer, ad);
+    delay(400);
+    tone(buzzer, gd);
+    delay(200);
+    tone(buzzer, g);
+    delay(400);
+    tone(buzzer, f);
+    delay(200);
+    tone(buzzer, c2);
+    delay(400);
+    tone(buzzer, c);
+    delay(800);
+    tone(buzzer, f);
+    delay(1600);
+    
+//Söz    : Mehmet Akif Ersoy
+//Beste  : Osman Zeki Üngör
+
+
+    noTone(buzzer);
+    delay(300);
 };
  
-extern motorclass motor; // aynı değişkeni farklı dosyalarda kullanmak için
+extern buzzer_h buzzer; // aynı değişkeni farklı dosyalarda kullanmak için
  
 #endif
